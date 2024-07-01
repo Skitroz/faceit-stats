abc8033aa798d460fb84bfb33847b23d0eda8c23
Mathéo <65279654+Skitroz@users.noreply.github.com>
2024-07-01 16:53:06 +0200

    🔥

--- app/joueur/page.tsx
+++ app/joueur/page.tsx
@@ -14,7 +14,6 @@ const Plus = () => (
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
     color="whiteLowEmphasis"
-    hovercolor="whiteLowEmphasis"
     height="24"
     width="24"
     class="sc-klVQfs fLZPES"
